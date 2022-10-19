977760a845136bc1d99036a091d13bb982afc9fe
yjyang027 <yjyang027@tamu.edu>
2022-10-18 21:38:52 -0500

    update ncorr loss

--- src/loss.py
+++ src/loss.py
@@ -60,25 +60,26 @@ class GaussNLLLoss(Loss_):
 class NCorrLoss(Loss_):
     """Negative correlation loss.
 
-    Precondition:
-    y_true.mean(axis=1) == 0
-    y_true.std(axis=1) == 1
-
-    Returns:
-    -1 = perfect positive correlation
-    1 = totally negative correlation
     """
 
     def __init__(self):
         super(NCorrLoss, self).__init__()
 
+    @staticmethod
+    def tile(y, method:str = "mean"):
+        if method == "mean":
+            return torch.tile(y.mean(dim=1).unsqueeze(1), (1, y.shape[1]))
+        if method == "norm":
+            return torch.tile(y.norm(dim=1).unsqueeze(1), (1, y.shape[1]))
+
     def forward(self, pred_y, y):
-        my = torch.mean(pred_y, dim=1)
-        my = torch.tile(torch.unsqueeze(my, dim=1), (1, y.shape[1]))
-        ym = pred_y - my
-        r_num = torch.sum(torch.multiply(y, ym), dim=1)
-        r_den = torch.sqrt(
-            torch.sum(torch.square(ym), dim=1) * float(y.shape[-1])
-        )
-        r = torch.mean(r_num / r_den)
-        return -r
+        pred_n = pred_y - self.tile(pred_y)
+        target_n = y - self.tile(y)
+        pred_n = pred_n / self.tile(pred_n, method="norm")
+        target_n = target_n / self.tile(target_n, method="norm")
+        r = (pred_n * target_n).sum(dim=1)
+        r = r.mean()
+        # z = torch.cat([pred_y, y], dim=0)
+        # r = torch.corrcoef(z)[:pred_y.shape[0], pred_y.shape[0]:].diagonal().mean() # off-diagnoal corr
+        # print("r:", r)
+        return 1-r
