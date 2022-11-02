95f237f164cfc81afbd47d6c73a33b3bdb8f8324
qwerty239qwe <qwerty239qwe@gmail.com>
2022-11-03 01:54:03 +0800

    add preprocessing

--- src/ml_models/train.py
+++ src/ml_models/train.py
@@ -2,9 +2,60 @@ import argparse
 from pathlib import Path
 import scanpy as sc
 import pandas as pd
+import numpy as np
+import joblib
 
 from regressions import *
 from utils import *
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import TruncatedSVD
+from sklearn.pipeline import Pipeline
+
+
+def preprocess(train_X,
+               test_X,
+               train_X_path,
+               test_X_path,
+               pipeline_path) -> None:
+    fitted = False
+    if Path(pipeline_path).is_file():
+        print("loading fitted preprocessor")
+        pl = joblib.load(pipeline_path)
+        fitted = True
+    else:
+        pl = Pipeline([("std", StandardScaler()),
+                       ("svd", TruncatedSVD(n_components=512, n_iter=20, random_state=1))])
+    if not Path(train_X_path).is_file() or not fitted:
+        if fitted:
+            train_X = pl.transform(train_X)
+        else:
+            print("fitting preprocessor")
+            train_X = pl.fit_transform(train_X)
+            joblib.dump(pl, pipeline_path)
+        with open(train_X_path, 'wb') as f:
+            np.save(f, train_X)
+    if not Path(test_X_path).is_file():
+        test_X = pl.transform(test_X)
+        with open(test_X_path, 'wb') as f:
+            np.save(f, test_X)
+
+
+def check_processed(train_X_raw,
+                    train_X_pps,
+                    test_X_raw,
+                    test_X_pps,
+                    pipeline_path,
+                    ):
+    if Path(train_X_pps).is_file() and Path(test_X_pps).is_file():
+        return
+    train_X = sc.read_h5ad(train_X_raw).X.toarray() if not Path(train_X_pps).is_file() else None
+    test_X = sc.read_h5ad(test_X_raw).X.toarray() if not Path(test_X_pps).is_file() else None
+    preprocess(train_X,
+               test_X,
+               train_X_path=train_X_pps,
+               test_X_path=test_X_pps,
+               pipeline_path=pipeline_path)
 
 
 def train(io_config,
@@ -17,13 +68,21 @@ def train(io_config,
     assert Path(io_config["input_training_y"]).is_file()
     assert Path(io_config["input_test_x"]).is_file()
 
+    check_processed(train_X_raw=Path(io_config["input_training_x"]),
+                    train_X_pps=Path(io_config["input_pps_training_x"]),
+                    test_X_raw=Path(io_config["input_test_x"]),
+                    test_X_pps=Path(io_config["input_pps_test_x"]),
+                    pipeline_path=Path(io_config["input_preprocessor_x"]),
+                    )
+
     for s in range(n_split):
         # data loading
-        train_X = sc.read_h5ad(io_config["input_training_x"]).X
+        with open(io_config["input_pps_training_x"], 'rb') as f:
+            train_X = np.load(f)
         n_cells, n_ftrs = train_X.shape
         sample_idx = np.random.choice(n_cells, n_cells // n_split)
 
-        train_X = train_X[sample_idx, :].toarray()
+        train_X = train_X[sample_idx, :]
         train_y = sc.read_h5ad(io_config["input_training_y"]).X[sample_idx, :].toarray()
 
         output_dir = Path(io_config["output_dir"])
@@ -45,10 +104,11 @@ def train(io_config,
         del train_y
 
     # prediction
-    test_X = sc.read_h5ad(io_config["input_test_x"])
+    with open(io_config["input_pps_test_x"], 'rb') as f:
+        test_X = np.load(f)
 
     for i, clf in enumerate(cv_clfs):
-        predicted_y = clf.predict(test_X.X.toarray())
+        predicted_y = clf.predict(test_X)
         pd.DataFrame(data=predicted_y).to_csv(output_dir / f"predicted_y_{i}.csv")
 
     del test_X
@@ -62,14 +122,19 @@ def train(io_config,
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     cfd = Path(__file__).resolve().parent / "configs"
-    model_cfd = cfd / "models"
 
     parser.add_argument("-m", "--model", help="model config file name or location")
+    parser.add_argument("-p", "--problem", default="cite")
     parser.add_argument("-i", "--io", help="IO config file name or location", default="test_io_cfg")
     parser.add_argument("-c", "--cv", help="cv config file name or location", default="cv_cfg")
     parser.add_argument("-n", "--num", help="Number of bootstrap samplings", default=5, type=int)
 
     args = parser.parse_args()
+    assert args.problem in ["cite", "multi"]
+    model_cfd = cfd / f"models_for_{args.problem}"
+
+    print("Config dir located: ", model_cfd.is_dir())
+
     model_configs = load_config((model_cfd / args.model).with_suffix(".yml")) \
         if (model_cfd / args.model).with_suffix(".yml").is_file() else load_config(args.model)
     io_configs = load_config((cfd / args.io).with_suffix(".yml")) \
